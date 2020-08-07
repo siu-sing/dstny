bb01e69281c45e67b18c85be7440f42c93ba8b95
andicodetrf <andrea_lau92@hotmail.com>
2020-08-07 22:45:47 +0800

    set up questions and user

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,40 @@
+class QuestionsController < ApplicationController
+
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
+  
+  before_action :set_question, only: [:show, :edit, :destroy, :update]
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  # def question_params
+  #   params.require(:question).permit(:name, :hometown, :location, :year, :age)
+  # end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # get 'questions/index'
+  # get 'questions/new'
+  # get 'questions/create'
+  # get 'questions/edit'
+  # get 'questions/update'
+  # get 'questions/show'
+  # get 'questions/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resources :questions do
+    resources :comments
+  end
+
+  root 'questions#index'
+
 end
+
+
