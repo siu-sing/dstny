a042e74abd70b850cce2a2deb5921c93956b620d
JiaHern <hojiahern@gmail.com>
2020-08-08 16:48:10 +0800

    removed profile

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2020_08_07_104204) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
