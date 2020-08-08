f26462871e550af25d65dc971738f6295c06829e
randallalala <randall.lim.lim.lim@hotmail.com>
2020-08-08 14:57:27 +0800

    layout view, route, gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/new'
+  get 'user/create'
+  get 'user/edit'
+  get 'user/update'
+  get 'user/show'
+  get 'user/destroy'
+#   get 'user/index'
+#   get 'user/new'
+#   get 'user/create'
+#   get 'user/edit'
+#   get 'user/update'
+#   get 'user/show'
+#   get 'user/destroy'
   # get 'questions/index'
   # get 'questions/new'
   # get 'questions/create'
@@ -9,6 +23,16 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  
+  resources :users
+
+  resources :users do
+    resources :questions
+  end
+
+  resources :users do
+    resources :comments
+  end
 
   resources :questions do
     resources :comments
