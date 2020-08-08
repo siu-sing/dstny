e60344a1809ab6b0c7aa5e233546b35553edcca5
siu-sing <lai.siusing@gmail.com>
2020-08-08 23:55:13 +0800

    added avatar on profile page, added avatars on question cards (both index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :profile, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :profile, :avatar])
   end
 end
