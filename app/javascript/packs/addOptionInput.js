f9a97fdaa8fb7b2a950916916e6c70af63cb65c9
andicodetrf <andrea_lau92@hotmail.com>
2020-08-10 23:28:12 +0800

    revised dynamic form-rows & added delete row button

--- app/javascript/packs/addOptionInput.js
+++ app/javascript/packs/addOptionInput.js
@@ -1,22 +1,61 @@
 console.log('test')
 
-const addOptionInput = ()=> {
-  console.log('does it go here?')
-// $(document).ready(function () {
-  const createButton = document.getElementById("addBook");
-  createButton.addEventListener("click", () => {
+function addOptionInput(){
 
-    const lastId = document.querySelector("#fieldsetContainer").lastElementChild.id; //to_get the last created child id
-
-    const newId = parseInt(lastId, 10) + 1; //incremenet to get unique id  
-
-    //to get previous id-1 fieldset and replace with 2
-    const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId);
-
-    document.querySelector("#fieldsetContainer").insertAdjacentHTML(
-        "beforeend", newFieldset
-    );
+  $("#addMore").on("click", function(e) {
+    console.log('test')
+    e.preventDefault(); //kill default action
+    let numberIdOfRow = $(".listForm .row:last-child").attr("data-id");
+    let num = parseInt(numberIdOfRow) + 1;
+  
+  
+    let html = `<div class="row mb-3" data-id="${num}">
+                <div class="col-md-11">
+                    <input class="form-control mb-2" type="text" name="question[options_attributes][${num}][option_text]" id="question_options_attributes_${num}_option_text" placeholder="Option (Required)">
+                </div>
+                <div class="col-md-1 stayEnd"><button class="btn btn-danger del"> x</button></div>
+                </div>`;
+  
+    $(".listForm").append(html);
+    console.log('added'+num+' - '+numberIdOfRow)
   });
-// });
-}
-export { addOptionInput }
+  
+  $(".listForm").on("click", ".del", function(e) {
+    e.preventDefault();
+    /*
+        Find the parent of the parent of the del button 
+        and completely remove it from the html
+    */
+    $(this)
+      .parent()
+      .parent()
+      .remove();
+      console.log('del')
+  });
+  };
+  
+  export { addOptionInput }
+  
+
+  //------------ SUPERSEDED CODES ---------------
+
+// const addOptionInput = ()=> {
+//   console.log('does it go here?')
+// // $(document).ready(function () {
+//   const createButton = document.getElementById("addBook");
+//   createButton.addEventListener("click", () => {
+
+//     const lastId = document.querySelector("#fieldsetContainer").lastElementChild.id; //to_get the last created child id
+
+//     const newId = parseInt(lastId, 10) + 1; //incremenet to get unique id  
+
+//     //to get previous id-1 fieldset and replace with 2
+//     const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId);
+
+//     document.querySelector("#fieldsetContainer").insertAdjacentHTML(
+//         "beforeend", newFieldset
+//     );
+//   });
+// // });
+// }
+// export { addOptionInput }
