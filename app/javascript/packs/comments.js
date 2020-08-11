75378224b6525dc262ac6e0cc5a15a2f59364a3b
JiaHern <hojiahern@gmail.com>
2020-08-11 15:35:02 +0800

    included edit button

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -1,19 +1,11 @@
 $(document).on("turbolinks:load", function () {
-    $(`.edit-button`).click(function (e) {
-        e.preventDefault()
-        e.stopPropagation()
-<<<<<<< HEAD
-
-        console.log("here")
-        // if got bugs, comment the .hide() first then use background colour to ensure select correct item
-        $(this).parent().prev().prev().show()
-        $(this).hide() // hides button
-        $(this).parent().prev().prev().prev().hide() // hides comment
-
-=======
-        $(this).parent().prev().css("display", "block")
-        $(this).hide()
-        $(this).parent().prev().prev().hide()
->>>>>>> included edit button
-    })
+        $(`.edit-button`).click(function (e) {
+                // if got bugs, comment the .hide() first then use background colour to ensure select correct item
+                e.preventDefault()
+                e.stopPropagation()
+                // $(this).parent().prev().css("display", "block")
+                $(this).hide()
+                // $(this).parent().prev().prev().hide()
+                document.querySelector(".edit_comment").style.display = "block";
+        })
 })
