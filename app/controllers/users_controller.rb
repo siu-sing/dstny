ed9c82aff826ac4244b7f20edbeb7d321a9e1037
siu-sing <lai.siusing@gmail.com>
2020-08-11 18:20:25 +0800

    user can view results of polls from the user/show page, only OPs can see who voted for which option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  
+
+    if @user.questions.any?
+      #for each of the question the user has
+      @user.questions.each do |question|
+        # for each of the option in each question
+        question.options.each do |option|
+          # Replace the option.voters array with the username of the voters
+          option.voters.each_with_index do |voter, idx|
+            option.voters[idx] = User.find(voter).username
+          end
+        end
+      end
+    end
   end
 
   def destroy
