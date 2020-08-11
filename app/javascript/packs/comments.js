2e9630e86854fe5e659157a375d29aa0769f8a23
JiaHern <hojiahern@gmail.com>
2020-08-11 15:35:02 +0800

    included edit button

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -2,6 +2,7 @@ $(document).on("turbolinks:load", function () {
     $(`.edit-button`).click(function (e) {
         e.preventDefault()
         e.stopPropagation()
+<<<<<<< HEAD
 
         console.log("here")
         // if got bugs, comment the .hide() first then use background colour to ensure select correct item
@@ -9,5 +10,10 @@ $(document).on("turbolinks:load", function () {
         $(this).hide() // hides button
         $(this).parent().prev().prev().prev().hide() // hides comment
 
+=======
+        $(this).parent().prev().css("display", "block")
+        $(this).hide()
+        $(this).parent().prev().prev().hide()
+>>>>>>> included edit button
     })
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   #   get 'categories/index'
   #   get 'categories/new'
   #   get 'categories/create'
@@ -8,6 +9,26 @@ Rails.application.routes.draw do
   #   get 'categories/destroy'
   get "categories/index"
 
+=======
+<<<<<<< HEAD
+#   get 'categories/index'
+#   get 'categories/new'
+#   get 'categories/create'
+#   get 'categories/edit'
+#   get 'categories/update'
+#   get 'categories/show'
+#   get 'categories/destroy'
+get 'categories/index'
+
+  get "comments/index"
+  get "comments/create"
+  get "comments/new"
+  get "comments/update"
+  get "comments/destroy"
+  get "comments/edit"
+  get "comments/show"
+=======
+>>>>>>> included edit button
   # get "comments/index"
   # get "comments/create"
   # get "comments/new"
@@ -15,6 +36,10 @@ Rails.application.routes.draw do
   # get "comments/destroy"
   # get "comments/edit"
   # get "comments/show"
+<<<<<<< HEAD
+=======
+>>>>>>> included edit button
+>>>>>>> included edit button
   # get 'questions/index'
   # get 'questions/new'
   # get 'questions/create'
@@ -59,6 +84,7 @@ Rails.application.routes.draw do
       patch :edit
     end
   end
+
 
   root "questions#index"
 end
