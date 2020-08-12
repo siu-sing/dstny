c05bffb72c350547d6870f17202bd6fd85f98622
JiaHern <hojiahern@gmail.com>
2020-08-12 16:50:02 +0800

    changed comments.js

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -1,15 +1,15 @@
 $(document).on("turbolinks:load", function () {
-    $(`.edit-button`).click(function (e) {
-        // if got bugs, comment the .hide() first then use background colour to ensure select correct item
-        e.preventDefault()
-        e.stopPropagation()
-        // $(this).parent().prev().css("display", "block")
+        $(`.edit-button`).click(function (e) {
+                // if got bugs, comment the .hide() first then use background colour to ensure select correct item
+                e.preventDefault()
+                e.stopPropagation()
+                // $(this).parent().prev().css("display", "block")
 
-        $(this).hide() ///this to hide the "edit button" on the original show page
-        let id = $(this).data('id')
-        console.log(id)
-        // $(this).parent().prev().prev().hide()
-        // console.log(document.querySelector(`#edit_comment_${id}`))
-        document.querySelector(`#edit_comment_${id}`).style.display = "block"; //to show the "edit button" from the partial _editcomment
-    })
+                $(this).hide() ///this to hide the "edit button" on the original show page
+                let id = $(this).data('id')
+                console.log(id)
+                // $(this).parent().prev().prev().hide()
+                // console.log(document.querySelector(`#edit_comment_${id}`))
+                document.querySelector(`#edit_comment_${id}`).style.display = "block"; //to show the "edit button" from the partial _editcomment
+        })
 })
