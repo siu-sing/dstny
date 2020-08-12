24b9bb79ce775e1e6ad0fca09a72bd321eefea69
JiaHern <hojiahern@gmail.com>
2020-08-12 12:30:44 +0800

    fixed edit button

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -2,16 +2,12 @@ $(document).on("turbolinks:load", function () {
     $(`.edit-button`).click(function (e) {
         e.preventDefault()
         e.stopPropagation()
-<<<<<<< HEAD
+
         console.log("here")
         // if got bugs, comment the .hide() first then use background colour to ensure select correct item
         $(this).parent().prev().prev().show()
         $(this).hide() // hides button
         $(this).parent().prev().prev().prev().hide() // hides comment
-=======
-        $(this).parent().prev().css("display", "block")
-        $(this).hide()
-        $(this).parent().prev().prev().hide()
->>>>>>> b53af1e... included edit button
+
     })
 })
