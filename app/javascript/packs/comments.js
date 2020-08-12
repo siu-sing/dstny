94040d2e8c5e7f791b6d00a4a0b7c744eaef6ee3
JiaHern <hojiahern@gmail.com>
2020-08-12 16:50:02 +0800

    changed comments.js

--- app/javascript/packs/comments.js
+++ app/javascript/packs/comments.js
@@ -1,11 +1,11 @@
 $(document).on("turbolinks:load", function () {
-        $(`.edit-button`).click(function (e) {
-                // if got bugs, comment the .hide() first then use background colour to ensure select correct item
-                e.preventDefault()
-                e.stopPropagation()
-                // $(this).parent().prev().css("display", "block")
-                $(this).hide()
-                // $(this).parent().prev().prev().hide()
-                document.querySelector(".edit_comment").style.display = "block";
-        })
+    $(`.edit-button`).click(function (e) {
+        // if got bugs, comment the .hide() first then use background colour to ensure select correct item
+        e.preventDefault()
+        e.stopPropagation()
+        // $(this).parent().prev().css("display", "block")
+        $(this).hide()
+        // $(this).parent().prev().prev().hide()
+        document.querySelector(".edit_comment").style.display = "block";
+    })
 })
